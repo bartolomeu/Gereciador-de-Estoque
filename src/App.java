381bba191ecf98c90c84e2be7796211ffb612682
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2024-10-05 18:29:07 -0300

    dividing the tasks

--- src/App.java
+++ src/App.java
@@ -2,4 +2,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    //tela home
+
+    //tela de cadastrar produto
+
+    //cadastrar produto
+
+    //listar produtos
+
+    //tela exibir produtos estoque
+
+    //tela atualizar quantidade produto
+
+    //atualizar qtd de estoque do produto
+
+    //tela registrar saidas
+    
+    //registrar saidas
+
+
+
 }
